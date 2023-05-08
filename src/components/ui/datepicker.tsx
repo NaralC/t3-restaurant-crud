e7061029fde73bfdbe3891b5a2d9c95f997aad9f
Naral <77269201+NaralC@users.noreply.github.com>
2023-05-08 01:24:19 +0100

    Date picker component
    - Lets you pick hh:mm after you pick the date
    - Logs out the time when clicked

---
+++ src/components/ui/datepicker.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import * as React from "react";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { cn } from "~/lib/utils";
+import { Button } from "~/components/ui/button";
+import { Calendar } from "~/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
+import { add, format } from "date-fns";
+import { INTERVAL, STORE_CLOSE_TIME, STORE_OPEN_TIME } from "~/constants/config";
+
+export function DatePickerDemo() {
+  const [date, setDate] = React.useState<Date | undefined>();
+
+  const getTimes = (): Date[] | undefined => {
+    if (!date) {
+      return;
+    }
+
+    const beginning = add(date.getDate(), { hours: STORE_OPEN_TIME - 1 });
+    const end = add(date.getDate(), { hours: STORE_CLOSE_TIME - 1 });
+
+    const times = [];
+
+    for (
+      let idx = beginning;
+      idx <= end;
+      idx = add(idx, { minutes: INTERVAL })
+    ) {
+      times.push(idx);
+    }
+
+    return times;
+  };
+
+  const times = getTimes();
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"default"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="w-4 h-4 mr-2" />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        {!date ? (
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            initialFocus
+          />
+        ) : (
+          <div className="flex-wrap">
+            {times?.map((time, idx) => {
+              return (
+                <div className="border-2 border-black" key={`time-${idx}`}>
+                  <button
+                    onClick={() => {
+                      const newDate = new Date(date);
+                      newDate.setHours(time.getHours(), time.getMinutes());
+                      setDate(newDate);
+                    }}
+                  >
+                    {format(time, "kk:mm")}
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </PopoverContent>
+    </Popover>
+  );
+}
---
+++ src/constants/config.ts
@@ -0,0 +1,3 @@
+export const STORE_OPEN_TIME = 9; // 9 am
+export const STORE_CLOSE_TIME = 17; // 5 pm
+export const INTERVAL = 30 // in minutes

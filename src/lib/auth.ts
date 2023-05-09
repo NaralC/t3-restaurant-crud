cb7c31bd6e077988ac96c5ed025612aefdbae5cd
Naral <77269201+NaralC@users.noreply.github.com>
2023-05-10 00:20:23 +0100

    Authentication via middleware
    - admins get redirected to dashboard should they try to access login
    - non-admins get redirected to login should they try to access dashboard

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,10 @@
+import { jwtVerify } from "jose";
+
+interface UserJwtPayload {
+    jti: string;
+    iat: number;
+}
+
 export const getJwtSecret = (): string => {
     const secret = process.env.JWT_SECRET_KEY;
 
@@ -6,4 +13,13 @@ export const getJwtSecret = (): string => {
     }
 
     return secret;
+}
+
+export const verifyAuth = async (token: string) => {
+    try {
+        const verified = await jwtVerify(token, new TextEncoder().encode(getJwtSecret()))
+        return verified.payload as UserJwtPayload;
+    } catch (error) {
+        throw new Error("Invalid token");
+    }
 }
---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { verifyAuth } from "./lib/auth";
+
+export async function middleware(req: NextRequest) {
+    // Get token from user
+    const token = req.cookies.get("user-token")?.value
+
+    // Check if user's authenticated
+    const verifiedToken = token && (await verifyAuth(token).catch((err) => {
+        console.log(err);
+    }))
+
+    // User trying to go to login without token, that's good
+    if (req.nextUrl.pathname.startsWith('/login') && !verifiedToken) {
+        return;
+    } 
+
+    const url = req.url;
+
+    if (url.includes("/login") && verifiedToken) {
+        return NextResponse.redirect(new URL("/dashboard", req.url))
+    }
+
+    if (!verifiedToken) {
+        return NextResponse.redirect(new URL("/login", req.url))
+    }
+}
+
+export const config = {
+    matcher: ['/dashboard', '/login']
+}

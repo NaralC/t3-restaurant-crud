036bc698ffd766f4c71ba3fed3e66bcbaf258871
Naral <77269201+NaralC@users.noreply.github.com>
2023-05-12 02:05:33 +0100

    Prisma schema

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,3 +1,6 @@
 export const STORE_OPEN_TIME = 9; // 9 am
 export const STORE_CLOSE_TIME = 17; // 5 pm
 export const INTERVAL = 30 // in minutes
+export const MAX_FILE_SIZE = 1024 * 1024 * 5 // 5 MB
+
+export const categories = ['all', 'breakfast', 'lunch', 'dinner'] as const
